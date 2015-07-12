ce47c727fce00a08574e49ec27cdf8d5383642dc
Raph <rbleuzet@gmail.com>
2015-07-12 22:02:26 +0200

    Get method fixed

--- HttpRequest.php
+++ HttpRequest.php
@@ -52,6 +52,9 @@ class HttpRequest
         $this->_enableCurl();
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->_url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $output = curl_exec($ch);
         curl_close($ch);
         return $output;
